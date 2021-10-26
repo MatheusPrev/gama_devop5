dd8f13d1eb0b767c29ac5466231dfaddcf22b262
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 15:27:21 -0300

    Update main.tf

--- single-master/terraform/main.tf
+++ single-master/terraform/main.tf
@@ -92,19 +92,19 @@ resource "aws_security_group" "acessos_master" {
       security_groups : null,
       self : null
     },
-    #{
-    #  cidr_blocks      = []
-    #  description      = ""
-    #  from_port        = 0
-    #  ipv6_cidr_blocks = []
-    #  prefix_list_ids  = []
-    #  protocol         = "tcp"
-    #  security_groups = [
-    #    "sg-0d7dbefa8a3d002d7",
-    #  ]
-    #  self    = false
-    #  to_port = 65535
-    #},
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups = [
+        "sg-016f5d475a2ed4e5f",
+      ]
+      self    = false
+      to_port = 65535
+    },
   ]
 
   egress = [
