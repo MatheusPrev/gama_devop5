3b1b6443546a3e406bbe73766f9841c036faa293
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 16:23:50 -0300

    Update ami_deploy.sh

--- single-master/ami_deploy.sh
+++ single-master/ami_deploy.sh
@@ -1,5 +1,12 @@
 pwd
-cd single-master/ami-terraform
+cd single-master/terraform
+/usr/local/bin/terraform output
+
+ID_MAQUINA=$(/usr/local/bin/terraform output | grep id_ami | awk '{print $2;exit}')
+
+cd ../
+
+cd ami-terraform
 /usr/local/bin/terraform init
 /usr/local/bin/terraform fmt
 /usr/local/bin/terraform apply -var="resource_id=${ID_MAQUINA/\",/}" -auto-approve
