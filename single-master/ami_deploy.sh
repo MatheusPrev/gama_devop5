855099102b6e6ba1295677d0bdbd4cebe2d61909
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 16:30:01 -0300

    Update ami_deploy.sh

--- single-master/ami_deploy.sh
+++ single-master/ami_deploy.sh
@@ -3,7 +3,7 @@ cd single-master/terraform
 /usr/local/bin/terraform output
 
 ID_MAQUINA=$(/usr/local/bin/terraform output | grep id_ami | awk '{print $2;exit}')
-
+echo "=================[$ID_MAQUINA]===============" 
 cd ../
 
 cd ami-terraform
