688500f0d431052755c2b4c32a4c74130b12a478
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 11:40:55 -0300

    Update deploy.sh

--- single-master/deploy.sh
+++ single-master/deploy.sh
@@ -15,7 +15,7 @@ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
 cd /home/ubuntu/gama_devop5/single-master/terraform
 /usr/local/bin/terraform output
 
-echo $"Agora somente abrir a URL: http://$(/usr/local/bin/terraform output | grep public | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
+echo $"Agora somente abrir a URL: http://$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
 
 ID_MAQUINA=$(/usr/local/bin/terraform output | grep id | awk '{print $2;exit}')
 echo ${ID_MAQUINA/\",/}
