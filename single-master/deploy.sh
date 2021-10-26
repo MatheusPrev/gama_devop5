40e6a489f70463408d01e194eb28c768b072c4e9
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 16:25:13 -0300

    Update deploy.sh

--- single-master/deploy.sh
+++ single-master/deploy.sh
@@ -26,6 +26,3 @@ cd terraform
 /usr/local/bin/terraform output
 
 echo $"Agora somente abrir a URL: http://$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
-
-ID_MAQUINA=$(/usr/local/bin/terraform output | grep id_ami | awk '{print $2;exit}')
-echo ${ID_MAQUINA/\",/}
