bcc1d8e4e75550543e34555c7dcdecf7157a1986
Renato <renatodufloth@hotmail.com>
2021-10-26 19:45:23 +0000

    novo ami_deploy.sh

--- single-master/ami_deploy.sh
+++ single-master/ami_deploy.sh
@@ -1,16 +1,1 @@
-pwd
-cd single-master/terraform
-/usr/local/bin/terraform output
-
-ID_MAQUINA=$(/usr/local/bin/terraform output | grep id_ami | awk '{print $2;exit}')
-echo "=================[$ID_MAQUINA]===============" 
-
-ID_MAQUINA=${ID_MAQUINA/\",/}
-echo "=================[$ID_MAQUINA]===============" 
-
-cd ../
-cd ami-terraform
-/usr/local/bin/terraform init
-/usr/local/bin/terraform fmt
-/usr/local/bin/terraform apply -var="resource_id='$ID_MAQUINA'" -auto-approve 
-/usr/local/bin/terraform output
+teste
