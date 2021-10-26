e2747a0459f948705dc4e7583ff702cf137a4278
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 16:11:15 -0300

    Update deploy.sh

--- single-master/deploy.sh
+++ single-master/deploy.sh
@@ -1,4 +1,4 @@
-#cd /home/ubuntu/gama_devop5/single-master/terraform
+
 cd single-master/terraform
 /usr/local/bin/terraform init
 /usr/local/bin/terraform fmt
@@ -6,9 +6,8 @@ cd single-master/terraform
 
 
 
-#echo $"[ec2-jenkins]" >> /home/ubuntu/gama_devop5/single-master/ansible/hosts # cria arquivo
+
 echo "[ec2-jenkins]" > ../ansible/hosts # cria arquivo
-#echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> /home/ubuntu/gama_devop5/single-master/ansible/hosts # captura output faz split de espaco e replace de ",
 echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts
 
 echo "Aguardando criação de maquinas ..."
@@ -17,10 +16,10 @@ sleep 10 # 30 segundos
 cd ../
 pwd
 
-#cd /home/ubuntu/gama_devop5/single-master/ansible
+
 cd ansible
 ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/id_rsa
-#cd /home/ubuntu/gama_devop5/single-master/terraform
+
 cd ../
 cd terraform
 /usr/local/bin/terraform output
@@ -28,11 +27,4 @@ cd terraform
 echo $"Agora somente abrir a URL: http://$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}'):8080" | sed -e "s/\",//g"
 
 ID_MAQUINA=$(/usr/local/bin/terraform output | grep id_ami | awk '{print $2;exit}')
-#echo ${ID_MAQUINA/\",/}
-
-#cd /home/ubuntu/gama_devop5/single-master/ami-terraform
-#/usr/local/bin/terraform init
-#/usr/local/bin/terraform fmt
-#/usr/local/bin/terraform apply -var="resource_id=${ID_MAQUINA/\",/}" -auto-approve 
-#/usr/local/bin/terraform apply -var="resource_id=i-062b0b80ee1469f44" -auto-approve 
-#/usr/local/bin/terraform output
+echo ${ID_MAQUINA/\",/}
