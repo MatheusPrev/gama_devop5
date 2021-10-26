cb1fd8acadf5da5bd35680d5fadf3b925281b7fa
MatheusPrev <65192612+MatheusPrev@users.noreply.github.com>
2021-10-26 08:10:11 -0300

    Update main.tf

--- multi-master/terraform/main.tf
+++ multi-master/terraform/main.tf
@@ -213,7 +213,7 @@ output "aws_instance_e_ssh" {
 output "security_group_multi_master" {
   value = [
     for key, item in aws_security_group.acessos_master :
-    "master ${key + 1} - ${item.elb_sg}"
+    "master ${key + 1} - ${item.id}"
   ]
 }
   
@@ -221,6 +221,6 @@ output "security_group_multi_master" {
 output "security_group_multi_worker" {
   value = [
     for key, item in aws_security_group.acessos_workers :
-    "worker ${key + 1} - ${item.elb_sg}"
+    "worker ${key + 1} - ${item.id}"
   ]
 }
