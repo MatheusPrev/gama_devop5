62bdb11e03a24e6980b0239557eb36c88ec04066
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-26 10:57:47 -0300

    Update deploy.sh

--- single-master/deploy.sh
+++ single-master/deploy.sh
@@ -4,7 +4,7 @@ cd /home/ubuntu/gama_devop5/single-master/terraform
 /usr/local/bin/terraform apply -auto-approve
 
 echo $"[ec2-jenkins]" > cd /home/ubuntu/gama_devop5/single-master/ansible/hosts # cria arquivo
-echo "$(~/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../1-ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 30 # 30 segundos
