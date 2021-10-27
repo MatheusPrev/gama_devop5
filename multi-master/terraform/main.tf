3c90fe5df7bd8e37dac13135a8168fda320d94e3
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-27 12:00:59 -0300

    Update main.tf

--- multi-master/terraform/main.tf
+++ multi-master/terraform/main.tf
@@ -57,20 +57,21 @@ resource "aws_instance" "workers" {
   count                  = 3
 }
 
-resource "aws_instance" "k8s_proxy" {
+resource "aws_instance" "haproxy" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name      = "devop5_jenkins_out"
+  tags = {
+    Name = "devop5_multi_haproxy"
+  }
+  subnet_id                   = "subnet-0dbc6439c94e66d76"
   associate_public_ip_address = true
   root_block_device {
     encrypted = true
     volume_size = 20
   }
-  vpc_security_group_ids = ["${aws_security_group.acessos_workers.id}"]
-  tags = {
-    Name = "devop5_multi_haproxy"
-  }
 
+  vpc_security_group_ids = ["${aws_security_group.acessos_workers.id}"]
 }
 
 
