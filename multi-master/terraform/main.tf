935659f7da7e9c810bfa793e4e330e838f020a2e
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-27 13:17:36 -0300

    Update main.tf

--- multi-master/terraform/main.tf
+++ multi-master/terraform/main.tf
@@ -149,6 +149,17 @@ resource "aws_security_group" "acessos_master" {
     },
     {
       cidr_blocks      = []
+      description      = "Libera acesso k8s_masters"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+    {
+      cidr_blocks      = []
       description      = ""
       from_port        = 0
       ipv6_cidr_blocks = []
