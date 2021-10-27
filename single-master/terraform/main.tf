ca5315df35b575c26aad9d2baa93c633d1d972f4
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-27 09:42:38 -0300

    Update main.tf

--- single-master/terraform/main.tf
+++ single-master/terraform/main.tf
@@ -164,6 +164,18 @@ resource "aws_security_group" "acessos_workers" {
       prefix_list_ids  = []
       protocol         = "tcp"
       security_groups = null,
+      security_groups = ["${aws_instance.acessos_workers.id}",]
+      self    = false
+      to_port = 65535
+    },
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups = null,
       #security_groups = ["${aws_instance.acessos_master.id}","${aws_instance.acessos_workers.id}",]
       self    = false
       to_port = 65535
